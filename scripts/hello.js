cc2c343c0f9600d7abfd5ecb34119dce547687d1
Jabba1788 <44513933+Jabba1788@users.noreply.github.com>
2018-11-08 20:14:09 +1000

    initial commit

---
+++ scripts/hello.js
@@ -0,0 +1,19 @@
+//document.write("hello world");
+//console.log("it worked");
+// alternative to DOMContentLoaded event
+document.onreadystatechange = function () {
+    if (document.readyState === "interactive") {
+      initApplication();
+    }
+  }
+
+  function initApplication() {
+        let el = document.getElementById("RightOrWrong");
+        console.log(el);
+
+        el.addEventListener("change", function() {
+            console.log(el.checked);
+            let el2 = document.getElementById("result");
+            el2.innerHTML = "checked: " + el.checked;
+        }); 
+  }     
